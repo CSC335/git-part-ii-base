f59fdb9e3411680026a9c6ca97db61e0b696930e
Jeremy Mowery <jermowery@email.arizona.edu>
2016-02-17 14:25:59 -0700

    Set up partner files and MyList

---
+++ src/MyList.java
@@ -0,0 +1,31 @@
+/**
+ * A simple list, partner 1 should implement this using a link structure.
+ * Partner 2 should implement this using an array structure.
+ * You don't need to implement this fully, or consider edge cases, or test this.
+ * At the very least start each method with your particular structure.
+ * This is an example to demonstrate what happens when you and a partner make changes
+ * to the same file, it is not intended to evaluate your ability to write a data structure.
+ */
+public class MyList {
+	
+	/**
+	 * Adds to the list.
+	 * You don't need to implement this fully, just have something that is a start
+	 * using the appropriate structure.
+	 */
+	public void add(String element) {
+		// Partner 1 should write this using a link structure.
+		// Partner 2 should write this using an array structure.
+	}
+
+	/**
+	 * Removes the last element of the list.
+	 * You don't need to implement this fully, hust have something that is a start
+	 * using the appropriate structure.
+	 */
+	public void removeLast() {
+		// Partner 1 should write this using a link structure.
+		// Partner 2 should write this using an array structure.
+	}
+
+} 
